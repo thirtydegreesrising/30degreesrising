ce7c7f980b6b135735fa0347c88625e2a2ae5b7a
thirtydegreesrising <104980977+thirtydegreesrising@users.noreply.github.com>
2022-05-16 11:39:19 +0100

    third push

--- glacier_map/config.js
+++ glacier_map/config.js
@@ -1,287 +1,245 @@
 var config = {
-    style: 'mapbox://styles/branigan/cjz37rcb003ib1cr3s8rnkt2d',
-    accessToken: 'pk.eyJ1IjoibWJ4c29sdXRpb25zIiwiYSI6ImNrMm01aG9hdTBlZGwzbXQ1ZXVrNHNmejAifQ.QHQA0N6XPWddCXtvoODHZg',
+    style: 'mapbox://styles/ucfnhch/cl37fa2vr002114lamw4m6pyc',
+    accessToken: 'pk.eyJ1IjoidWNmbmhjaCIsImEiOiJja3lrazVpdmkyNWl2MnZzZXB3ZDB3NHA3In0._zxeE9Xr4_NYgQIwWS9x3w',
     showMarkers: false,
     theme: 'dark',
-    use3dTerrain: true,
-    title: 'Glaciers of Glacier National Park',
-    subtitle: 'Change in coverage from 1998 to 2015',
+    title: '',
+    subtitle: '',
     byline: '',
-    footer: 'Source: Story text from Wikipedia, August 2019. Data from <a href="https://www.usgs.gov/centers/norock/science/retreat-glaciers-glacier-national-park">USGS</a>',
+    footer: '',
     chapters: [
         {
-            id: 'glacier-np',
-            alignment: 'full',
-            title: 'Glacier National Park Glaciers',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg/800px-2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg',
-            description: 'Glacier National Park is dominated by mountains which were carved into their present shapes by the huge glaciers of the last ice age. These glaciers have largely disappeared over the last 12,000 years. Evidence of widespread glacial action is found throughout the park in the form of U-shaped valleys, cirques, arêtes, and large outflow lakes radiating like fingers from the base of the highest peaks. Since the end of the ice ages, various warming and cooling trends have occurred. The last recent cooling trend was during the Little Ice Age, which took place approximately between 1550 and 1850. During the Little Ice Age, the glaciers in the park expanded and advanced, although to nowhere near as great an extent as they had during the Ice Age.',
+            id: 'overall',
+            alignment: 'right',
+            title: 'Overall',
+            image: '',
+            description: 'Glacier rephotograph provides effective and objective evidence for visually comparing and assessing landscape changes over time. Scientific agency and volunteers contribute to the glacier rephotographs. Glacier rephotographs are usually conducted during Summer since bare ice and glacier fronts are exploed clearly without seasonal covering of snow. Here, photographs comparison and glacier boundaries for four representative galciers in Iceland, Svabard and Alaska are presented to glance how the climate change may impact on glaciers locally.',
             location: {
-                center: [-113.91666, 48.66451],
-                zoom: 8,
-                pitch: 0.00,
-                bearing: 0.00
+                center: [-10, 65],
+                zoom: 2,
+                pitch: 0,
+                bearing: 0
             },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [{
+                layer:'glacier',
+                opacity: 0,
+            }],
+            onChapterExit: []
+        },
+        {
+            id: 'iceland',
+            alignment: 'right',
+            title: 'Iceland',
+            image: '',
+            description: 'The glaciers and ice caps cover 11% of the land area of the whole Iceland (around 11,400 km² out of the total area of 103,125 km²). Iceland owns 13 major glaciers. Due to climate change, Iceland is unfortunately losing glaciers. And some glaciers are losing their glacier titles',
+            location: {
+                center: [-12, 65],
+                zoom: 5,
+                pitch: 20,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [{
+                layer:'glacier',
+                opacity: 0.8,
+            }
+            ],
+            onChapterExit: [
+
+            ]
+        },
+        {
+            id: 'iceland-1',
+            alignment: 'right',
+            title: 'Iceland - Breiðamerkurjökull',
+            image: 'Iceland.gif',
+            description: 'Breiðamerkurjökull is the outlet glacier of Vatna Glacier and the Mountain-front glacier in the south of Vatna Glacier National Park, Iceland.These images record the decline of Breiðamerkurjökull between 1982 and now. Credit: National Land Survey of Iceland/Dr Kieran Baxter, University of Dundee',
+            location: {
+                center: [-16.08, 64.1],
+                zoom: 9,
+                pitch: 20,
+                bearing: 40
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
             onChapterEnter: [
                 {
-                    layer: 'gnpglaciers-1998',
-                    opacity: 0.25
+                layer:'glacier',
+                opacity: 0.8,
                 },
                 {
-                    layer: 'glaciernp-boundary',
-                    opacity: 0.25
-                }
+                layer:'glacier-highlight',
+                opacity: 0.7,
+            }
             ],
             onChapterExit: [
+                
                 {
-                    layer: 'glaciernp-boundary',
-                    opacity: 0
+                layer:'glacier-highlight',
+                opacity: 0,
                 }
+
             ]
         },
+
         {
-            id: 'harrison1998',
+            id: 'svabard',
             alignment: 'left',
-            title: 'Harrison Glacier, 1998',
+            title: 'Svabard',
             image: '',
-            description: 'Harrison Glacier is located in the US state of Montana in Glacier National Park. Situated on a southeast facing ridge immediately south of Mount Jackson, Harrison Glacier is the largest glacier in Glacier National Park. Compared to many of the vanishing glaciers in Glacier National Park, Harrison Glacier has a much higher altitude accumulation zone (approximately 9,000 feet (2,700 m)) which has allowed it to maintain some equilibrium in its glacier mass balance.',
+            description: 'Approximately 60% of Svabard (about 36,502 km²) is covered with glaciers. Affected by global warming, significant climate change in Svalbard has occurred - average temperature increased by 4 degrees Celsius, and by 7 degrees in winter since 1970.',
             location: {
-                center: [-113.72917, 48.58938],
-                zoom: 12.92,
-                pitch: 39.50,
-                bearing: 36.00
+                center: [12.3, 78.2],
+                zoom: 4.8,
+                pitch: 30,
+                bearing: 0
             },
-            onChapterEnter: [],
-            onChapterExit: [
-                // {
-                //     layer: 'gnpglaciers-2015',
-                //     opacity: 0
-                // }
-            ]
-        },
-        {
-            id: 'harrison2015',
-            alignment: 'left',
-            title: 'Harrison Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Harrison Glacier lost 169 acres of surface area (about 37%).',
-            location: {
-                center: [-113.72917, 48.58938],
-                zoom: 12.92,
-                pitch: 39.50,
-                bearing: 36.00
-            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
             onChapterEnter: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
+                    layer:'glacier',
+                    opacity: 0.8,
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
                 }
             ]
         },
+
         {
-            id: 'blackfoot1998',
+            id: 'svabard-1',
             alignment: 'left',
-            title: 'Blackfoot Glacier, 1998',
-            image: '',
-            description: 'Blackfoot Glacier is the second largest of the remaining 25 glaciers in Glacier National Park, Montana. Blackfoot Glacier is just to the north of Blackfoot Mountain and near Jackson Glacier. When first documented in 1850, the glacier also included the now separate Jackson Glacier and together, they covered 1,875 acres (7.59 km2). In 1850, there were an estimated 150 glaciers in the park. Glaciologists have stated that by the year 2030, all the glaciers in the park may disappear.',
+            title: 'Svabard - Blomstrandbreen',
+            image: 'Blomstrandbreen.gif',
+            description: '1928 to 2002; Credit:B/W Image by Courtesy of Norwegian Polar Institute; Color Image by Christian Aslund/Greenpeace.',
             location: {
-                center: [-113.66573, 48.59181],
-                zoom: 12.92,
-                pitch: 51.50,
-                bearing: -26.40
+                center: [12.37, 79.065385],
+                zoom: 8,
+                pitch: 20,
+                bearing: 0
             },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'blackfoot2015',
-            alignment: 'left',
-            title: 'Blackfoot Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Blackfoot Glacier lost 31 acres of surface area (about 8%).',
-            location: {
-                center: [-113.66573, 48.59181],
-                zoom: 12.92,
-                pitch: 51.50,
-                bearing: -26.40
-            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
             onChapterEnter: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
+                    layer:'glacier',
+                    opacity: 0.8,
+                    },
+                    {
+                    layer:'glacier-highlight',
+                    opacity: 0.7,
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
+                    layer:'glacier-highlight',
+                    opacity: 0,
                 }
             ]
         },
         {
-            id: 'agassiz1998',
-            alignment: 'left',
-            title: 'Agassiz Glacier, 1998',
+            id: 'alaska',
+            alignment: 'right',
+            title: 'Alaska',
             image: '',
-            description: 'Agassiz Glacier is in Glacier National Park in the U.S. state of Montana. It is named after Louis Agassiz, a Swiss-American glaciologist. The glacier is situated in a cirque to the southeast of Kintla Peak west of the Continental Divide. Agassiz Glacier is one of several glaciers that have been selected for monitoring by the U.S. Geological Survey\'s Glacier Monitoring Research program, which is researching changes to the mass balance of glaciers in and surrounding Glacier National Park.',
+            description: '',
             location: {
-                center: [-114.15881, 48.93439],
-                zoom: 13.51,
-                pitch: 41.00,
-                bearing: 78.33
+                center: [-136, 60],
+                zoom: 4,
+                pitch: 30,
+                bearing: 0
             },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'agassiz2015',
-            alignment: 'left',
-            title: 'Agassiz Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Agassiz Glacier lost 108 acres of surface area (about 37%).',
-            location: {
-                center: [-114.15881, 48.93439],
-                zoom: 13.51,
-                pitch: 41.00,
-                bearing: 78.33
-            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
             onChapterEnter: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25,
+                    layer:'glacier',
+                    opacity: 0.8,
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
                 }
             ]
         },
+
         {
-            id: 'rainbow1998',
-            alignment: 'left',
-            title: 'Rainbow Glacier, 1998',
-            image: '',
-            description: 'Rainbow Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated immediately to the east of Rainbow Peak at an elevation between 8,500 feet (2,600 m) and 8,000 feet (2,400 m) above sea level. The glacier has visible crevasses in satellite imagery. Rainbow Glacier has shown modest retreat compared to other glaciers in Glacier National Park.',
+            id: 'alaska-1',
+            alignment: 'right',
+            title: 'Alaska - Muir',
+            image: 'Muir.gif',
+            description: '',
             location: {
-                center: [-114.08659, 48.88039],
-                zoom: 13.09,
-                pitch: 50.00,
-                bearing: -53.60
+                center: [-136.30, 59.17830],
+                zoom: 10,
+                pitch: 30,
+                bearing: 0
             },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'rainbow2015',
-            alignment: 'left',
-            title: 'Rainbow Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Rainbow Glacier lost 17 acres of surface area (about 6%).',
-            location: {
-                center: [-114.08659, 48.88039],
-                zoom: 13.09,
-                pitch: 50.00,
-                bearing: -53.60
-            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
             onChapterEnter: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
+                    layer:'glacier',
+                    opacity: 0.8,
+                },
+                {
+                    layer:'glacier-highlight',
+                    opacity: 0.7,
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
+                    layer:'glacier-highlight',
+                    opacity: 0,
                 }
             ]
         },
+
         {
-            id: 'kintla1998',
-            alignment: 'left',
-            title: 'Kintla Glacier, 1998',
-            image: '',
-            description: 'Kintla Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated on a plateau 2 miles (3.2 km) southwest of Kintla Peak at an elevation between 8,700 feet (2,700 m) and 7,700 feet (2,300 m) above sea level. The glacier has numerous crevasses and is actually two glaciers.',
+            id: 'alaska-2',
+            alignment: 'right',
+            title: 'Alaska - Lechner',
+            image: 'Lechner.gif',
+            description: '',
             location: {
-                center: [-114.18755, 48.92880],
-                zoom: 13.09,
-                pitch: 48.50,
-                bearing: 164.00
+                center: [-149.6, 59.94361],
+                zoom: 11,
+                pitch: 30,
+                bearing: 0
             },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'kintla2015',
-            alignment: 'left',
-            title: 'Kintla Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Harrison Glacier lost 24 acres of surface area (about 10%).',
-            location: {
-                center: [-114.18755, 48.92880],
-                zoom: 13.09,
-                pitch: 48.50,
-                bearing: 164.00
-            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
             onChapterEnter: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
+                    layer:'glacier',
+                    opacity: 0.8,
+                },               
+                {
+                    layer:'glacier-highlight',
+                    opacity: 0.7,
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
+                    layer:'glacier-highlight',
+                    opacity: 0,
                 }
             ]
         },
-        {
-            id: 'sperry1998',
-            alignment: 'left',
-            title: 'Sperry Glacier, 1998',
-            image: '',
-            description: 'Sperry Glacier is a glacier on the north slopes of Gunsight Mountain west of the Continental Divide in Glacier National Park in the U.S. state of Montana. Although many geologic features of Glacier National Park were formed during the much longer period of glaciation ending over 10,000 years ago, Sperry Glacier — like all the glaciers in the park today — is a product of the recent Little Ice Age, the period of cooler average temperatures starting in about the 13th century and concluding in the mid-19th century.',
-            location: {
-                center: [-113.75672, 48.62433],
-                zoom: 13.68,
-                pitch: 34.50,
-                bearing: 106.40
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'sperry2015',
-            alignment: 'left',
-            title: 'Sperry Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Harrison Glacier lost 37 acres of surface area (about 16%).',
-            location: {
-                center: [-113.75672, 48.62433],
-                zoom: 13.68,
-                pitch: 34.50,
-                bearing: 106.40
-            },
-            onChapterEnter: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
-                }
-            ]
-         }
+
+
     ]
 };
