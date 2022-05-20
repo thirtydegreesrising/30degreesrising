d7ce8788b93d0b348ada98fd94e8c3d4219713b5
thirtydegreesrising <104980977+thirtydegreesrising@users.noreply.github.com>
2022-05-20 13:04:28 +0100

    index-test-pushed

--- glacier_map/config.js
+++ glacier_map/config.js
@@ -13,7 +13,7 @@ var config = {
             alignment: 'right',
             title: 'Overall',
             image: '',
-            description: 'Glacier rephotograph provides effective and objective evidence for visually comparing and assessing landscape changes over time. Scientific agency and volunteers contribute to the glacier rephotographs. Glacier rephotographs are usually conducted during Summer since bare ice and glacier fronts are exposed clearly without seasonal covering of snow. Here, photographs comparison and glacier boundaries for four representative galciers in Iceland, Svabard and Alaska are presented to glance how the climate change may impact on glaciers locally. Glacier boundary used here are from version 6 of The Randolph Glacier Inventory (RGI) provided by NSIDC.',
+            description: 'Glacier rephotograph provides effective and objective evidence for visually comparing and assessing landscape changes over time. Scientific agency and volunteers contribute to the glacier rephotographs. Glacier rephotographs are usually conducted during Summer since bare ice and glacier fronts are exposed clearly without seasonal covering of snow. Here, photographs comparison and glacier boundaries for four representative galciers in Iceland, Svabard and Alaska are presented to glance how the climate change may impact on glaciers locally. Glacier boundary used here are from version 6 of the Randolph Glacier Inventory (RGI) provided by NSIDC.',
             location: {
                 center: [-10, 65],
                 zoom: 2,
@@ -34,7 +34,7 @@ var config = {
             alignment: 'right',
             title: 'Iceland',
             image: '',
-            description: 'The glaciers and ice caps cover 11% of the land area of the whole Iceland (around 11,400 km² out of the total area of 103,125 km²). Iceland owns 13 major glaciers. Due to climate change, Iceland is unfortunately losing glaciers. And some glaciers are losing their glacier titles',
+            description: 'The glaciers and ice caps cover 11% of the land area of the whole Iceland (around 11,400 km² out of the total area of 103,125 km²). Most of the Icelandic glaciers retreated rapidly during the warm decades from 1930 to 1960, slowing down as the climate cooled during the following decade, and started to advance after 1970. The rate of advance peaked in the 1980s, after which it slowed down until about 1990. As a consequence of rapid warming of the climate that has taken place since the mid-1980s, most glaciers in Iceland began to retreat after 1990, and by 2000 all monitored non-surge type glaciers in Iceland were retreating.',
             location: {
                 center: [-12, 65],
                 zoom: 5,
@@ -120,7 +120,7 @@ var config = {
             alignment: 'left',
             title: 'Svabard - Blomstrandbreen',
             image: 'Blomstrandbreen.gif',
-            description: 'Credit:B/W Image by Courtesy of Norwegian Polar Institute; Color Image by Christian Aslund/Greenpeace.',
+            description: 'Blomstrandbreen, a glacier in the King\'s Bay area of Spitsbergen, has retreated approximately 2 km (1.2 mi) in the past 80 years. Since 1960 the average retreat of Blomstrandbreen has been about 35 m (115 ft) a year, and this average was enhanced due to an accelerated rate of retreat since 1995. Credit:B/W Image by Courtesy of Norwegian Polar Institute; Color Image by Christian Aslund/Greenpeace.',
             location: {
                 center: [12.37, 79.065385],
                 zoom: 8,
@@ -142,6 +142,10 @@ var config = {
             ],
             onChapterExit: [
                 {
+                    layer:'glacier',
+                    opacity: 0,
+                },
+                {
                     layer:'glacier-highlight',
                     opacity: 0,
                 }
@@ -152,10 +156,10 @@ var config = {
             alignment: 'right',
             title: 'Alaska',
             image: '',
-            description: '',
+            description: '"A 2005 aerial survey of Alaskan coastal glaciers identified more than a dozen glaciers, many former tidewater and calving glaciers, including Grand Plateau, Alsek, Bear, and Excelsior Glaciers that are rapidly retreating.Of 2,000 glaciers observed, 99% are retreating." (Pelto, 2016)',
             location: {
                 center: [-136, 60],
-                zoom: 4,
+                zoom: 4.1,
                 pitch: 30,
                 bearing: 0
             },
---
+++ sea_level/city.js
@@ -0,0 +1,9 @@
+let city = [{
+    location: "Skagway",
+    latitude: "59.45",
+    longitude: "-135.327"
+}, {
+    location: "Juneau",
+    latitude: "58.298",
+    longitude: "-134.412"
+}]
